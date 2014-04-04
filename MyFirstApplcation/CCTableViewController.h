a1211d0dc400d539858774d37aff9ec395d2049a
Soo Kiat Ter <lucastersk@gmail.com>
2014-04-04 06:24:17 +0100

    added tableviewcon

---
+++ MyFirstApplcation/CCTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCTableViewController.h
+//  MyFirstApplcation
+//
+//  Created by Soo Kiat Ter on 04/04/2014.
+//  Copyright (c) 2014 Can Can. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCTableViewController : UITableViewController
+
+@end
